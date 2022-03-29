d38925abb28570a01e19a71138280cad2c20b643
spykelion <ndilionel6@gmail.com>
2022-03-29 18:38:53 +0100

    Create Todo Models, add TodoContext.cs Also add TodoDTO to TodoItem.cs

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -1,6 +1,15 @@
-﻿namespace TodoApi.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TodoApi.Models
 {
-    public class TodoContext
+    public class TodoContext : DbContext
     {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
     }
 }
